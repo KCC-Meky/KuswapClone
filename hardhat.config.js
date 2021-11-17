0c0b0273ee0ce5170595a3f0f137169d2f9d3f9f
unknown <chanmickyyun@gmail.com>
2021-11-17 21:34:52 +0100

    init commit

---
+++ hardhat.config.js
@@ -0,0 +1,52 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  networks: {
+    kcc: {
+      url: 'https://rpc-testnet.kcc.network',
+      accounts: ['']
+    }
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1,
+          }
+        }
+      },
+      {
+        version: "0.5.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1,
+          }
+        }
+      },
+      {
+        version: "0.8.0",
+        settings: {},
+      },
+    ],
+  },
+};
---
+++ scripts/sample-script.js
@@ -0,0 +1,46 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  //Deploy Wrapped Native token
+  const WKCS = await hre.ethers.getContractFactory("WKCS");
+  const wkcs = await WKCS.deploy(singAddress.address, 53, singAddress.address)
+  await wkcs.deployed()
+  console.log("wkcs deployed to:", wkcs.address);
+
+
+  //Deploy factory
+  const Factory = await hre.ethers.getContractFactory('KuswapFactory');
+  const factory = await Factory.deploy(singAddress.address)
+  await factory.deployed()
+  console.log("KUS factory :", factory.address)
+  console.log("INIT HASH :",await factory.INIT_CODE_HASH())
+
+  // change hash in Router then comment all previous and run 
+  // const wKcsAddres = ''
+  // const factoryAddress = ''
+  // const Router = await hre.ethers.getContractFactory('KuswapRouter02');
+  // const router = await Router.deploy(factoryAdd, wethAdd)
+  // await router.deployed()
+  // console.log("KUS Router :", router.address)  
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
